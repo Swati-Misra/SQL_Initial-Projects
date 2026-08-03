0ac92b33152cea2371b56d9b931ddf565b4aa1cb
Swati Misra <133936056+Swati-Misra@users.noreply.github.com>
2026-08-03 15:08:52 -0400

    Add files via upload

---
+++ Problems Beginner Set 1.sql
@@ -0,0 +1,23 @@
+-- Beginner Problems
+-- Query all columns for all American cities in the CITY table with populations larger than 100000. The CountryCode for America is USA.
+SELECT * From CITY
+WHERE CountryCode = 'USA' AND POPULATION >100000;
+
+-- Query the NAME field for all American cities in the CITY table with populations larger than 120000. The CountryCode for America is USA.
+SELECT Name From CITY
+WHERE CountryCode = 'USA' AND POPULATION >120000;
+
+-- Query all columns for a city in CITY with the ID 1661.
+SELECT * From CITY
+Where ID= 1661;
+
+-- Query the names of all the Japanese cities in the CITY table. The COUNTRYCODE for Japan is JPN.
+SELECT Name From CITY
+Where COUNTRYCODE = 'JPN';
+
+-- Query a list of CITY and STATE from the STATION table.
+SELECT CITY, STATE FROM STATION;
+
+-- Query a list of CITY names from STATION for cities that have an even ID number. Print the results in any order, but exclude duplicates from the answer.
+SELECT DISTINCT CITY FROM STATION
+WHERE ID % 2 = 0;
